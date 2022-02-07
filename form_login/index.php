cdeedabc6d0312d1ae71e9926fffb0d5e48c15a2
RuslanYakushenko <84664595+RuslanYakushenko@users.noreply.github.com>
2022-02-07 18:41:35 +0200

    Add files via upload

---
+++ form_login/index.php
@@ -0,0 +1,55 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My resume</title>
+    <!-- <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet"> -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+<body> 
+    <div class="conteiner">
+        <div class="form">
+            <h2 class="login"><span class="lo">Lo</span>gin</h2>
+            <form action="index.php">
+                <!-- <label for="name">Имя</label> -->
+                <div class="block_email">
+                    <i class="fa fa-envelope" style="font-size:15px;color:#7c2ae8"></i>
+                    <input class="line_email" type="" placeholder="Enter your email">
+                    
+                </div>
+                <div class="block_password">
+                    <i class="fa fa-lock" style="font-size:20px;color:#7c2ae8"></i>
+                    <input class="line_password" type="" placeholder="Enter your password">
+                </div>
+                <div class="block_forgot_password">
+                    <a class="forgot_password" href="">Forgot password?</a>
+                </div>
+                <div class="block_button">
+                    <button class="button">Login</button>
+                </div>
+            </form>
+            <div class="registration_proposal">
+                <span class="registration_proposal_text">Don't have an account?</span>
+                <a class="registration_proposal_url" href="">Signup now</a>
+            </div>
+        </div>
+        <div class="image">
+            <img src="img/746f2e45-80e6-4030-a4ba-4525a073e93e.jpeg" alt="" width="500px" height="auto"»> 
+                <div class="quote">
+                    <span class="quote_top">Every new friend is a new adventure</span>
+                    <p class="quote_bottom">Let's get connected</p>
+                </div>
+                <!-- <span class="quote_bottom">Let's get connected</span> -->           
+        </div>
+    </div>  
+</body>
+</html>
